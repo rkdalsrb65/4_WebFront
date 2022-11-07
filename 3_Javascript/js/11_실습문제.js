8732f885301b54acd9208336be3adb9a9608642e
rkdalsrb65 <rkdalsrb65@naver.com>
2022-11-08 00:46:16 +0900

    11_실습문제.html

--- 3_Javascript/js/11_실습문제.js
+++
@@ -1,8 +0,0 @@
-/* <form action="/member/signUp" method="POST" name="signUp-frm" id="signUp-frm"></form>
-액션/메소드
-체인지/키업
-
-submit
-회원가입 버튼 클릭 시 : validate() 함수를 호출하여
-<form onsubmit="return validate()"></form> */
-

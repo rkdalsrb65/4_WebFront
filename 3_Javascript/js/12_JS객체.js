353455fda1e08ee124aa1a09098e532a8b2386c2
rkdalsrb65 <rkdalsrb65@naver.com>
2022-11-09 14:01:18 +0900

    JS객체

--- 3_Javascript/js/12_JS객체.js
+++ 3_Javascript/js/12_JS객체.js
@@ -69,4 +69,16 @@ document.getElementById("btn1").addEventListener("click", function(){
 
     // 객체의 key 제거 방법
     // -> delete 객체변수명.key
+
+    // 객체명['key'] 방법으로
+    // 객체가 가지고 있는 모든 값 출력하기
+    // -> for in 구문(객체 전용 for문)
+    div1.innerHTML += "product['capacity'] : " + product['capacity'] + "<br>";
+
+    // for(let key in 객체명) : 매 바퀴마다 객체의 K를 하나씩 얻어와
+    //                          key 변수에 저장
+    for(let key in product){
+        // 객체의 K는 모두 string == 'pName', 'price'
+        div1.innerHTML += product[key] + "<br>"
+    }
 });
